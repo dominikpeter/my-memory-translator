c2dab7ff68cae38556b26a85947f8e449d1f8017
dominik <dominikpeter@users.noreply.github.com>
2019-04-28 15:35:11 +0200

    minor

--- MyMemory/MyMemory.py
+++ MyMemory/MyMemory.py
@@ -1,5 +1,8 @@
 import requests
 import html
+import re
+import time
+from datetime import timedelta
 
 class MyMemory:
     def __init__(self):
@@ -8,15 +11,34 @@ class MyMemory:
         self.json = {}
         self.translations = []
         self.scores = []
+        self.limit_reached = False
+        self.seconds_to_wait = 0
 
     def translate(self, sentence, source_lang, target_lang):
         self.params = {"q": sentence,
                        "langpair": f"{source_lang}|{target_lang}"}
         response = requests.get(self.url, params=self.params)
         self.json = response.json()
-        self.translations = [i['translation'] for i in self.json['matches']]
-        self.translations = [html.unescape(i) for i in self.translations]
-        self.scores = [self._parse_int(i['quality']) for i in self.json['matches']]
+        try:
+            warning = self.json['responseData']['translatedText']
+            self.limit_reached = warning.lower().startswith("mymemory warning")
+        except Exception as e:
+            self.limit_reached = False
+        if self.limit_reached:
+            #print("Limit reached")
+            self.translations = ['limit reached']
+            time_string = re.findall(
+                r"\d*\sHOURS\s\d*\sMINUTES\s\d*\sSECONDS", warning)[0]
+            ti = time.strptime(
+                "09 HOURS 49 MINUTES 28 SECONDS",
+                "%H HOURS %M MINUTES %S SECONDS")
+            self.seconds_to_wait = timedelta(hours=ti.tm_hour,
+                                             minutes=ti.tm_min,
+                                             seconds=ti.tm_sec).total_seconds()
+        else:
+            self.translations = [i['translation'] for i in self.json['matches']]
+            self.translations = [html.unescape(i) for i in self.translations]
+            self.scores = [self._parse_int(i['quality']) for i in self.json['matches']]
 
     def extract_first(self, score=True):
         translation = self.translations[0]
